ebc708c116e94637e10788d769f37cb62c2a71c1
Leonid <32189038+moguchev@users.noreply.github.com>
2019-09-04 20:15:35 +0300

    Update roman.js

--- source/roman.js
+++ source/roman.js
@@ -1,25 +1,25 @@
 'use strict';
 
 const base = [
-	{ value: 1000, char: 'M' },
-	{ value: 900, char: 'CM' },
-	{ value: 500, char: 'D' },
-	{ value: 400, char: 'CD' },
-	{ value: 100, char: 'C' },
-	{ value: 90, char: 'XC' },
-	{ value: 50, char: 'L' },
-	{ value: 40, char: 'XL' },
-	{ value: 10, char: 'X' },
-	{ value: 9, char: 'IX' },
-	{ value: 5, char: 'V' },
-	{ value: 4, char: 'IV' },
-	{ value: 1, char: 'I' }
+    { value: 1000, char: 'M' },
+    { value: 900, char: 'CM' },
+    { value: 500, char: 'D' },
+    { value: 400, char: 'CD' },
+    { value: 100, char: 'C' },
+    { value: 90, char: 'XC' },
+    { value: 50, char: 'L' },
+    { value: 40, char: 'XL' },
+    { value: 10, char: 'X' },
+    { value: 9, char: 'IX' },
+    { value: 5, char: 'V' },
+    { value: 4, char: 'IV' },
+    { value: 1, char: 'I' }
 ]
 
 function digitToRoman(number) {
-	if (!number) return '';
+    if (!number) return '';
 
-	return base.reduce((result, currentValue) => {
+    return base.reduce((result, currentValue) => {
         while (number >= currentValue.value) {
             result += currentValue.char;
             number -= currentValue.value;
@@ -30,27 +30,26 @@ function digitToRoman(number) {
 }
 
 function romanToDigit(str) {
-	if (typeof str !== "string") {
-		return;
+    if (typeof str !== "string") {
+       return;
+    }
+    str = str.toUpperCase();
+	
+    return base.reduce((result, currentValue) => {
+        while (currentValue.char === str.substr(0, currentValue.char.length)) {
+	    if (str.length !== 0) {
+		result +=  currentValue.value;
+		str = str.substr(0 + currentValue.char.length);
+	    }
 	}
-	str = str.toUpperCase();
-
-	return base.reduce((result, currentValue) => {
-		while (currentValue.char === str.substr(0, currentValue.char.length)) {
-			if (str.length !== 0) {
-				result +=  currentValue.value;
-				str = str.substr(0 + currentValue.char.length);
-			}
-		}
-		return  result;
-	}, 0 );
+	return  result;
+    }, 0 );
 }
 
 const roman = value => {
     const number = parseInt(value, 10);
     if (Number.isNaN(number)) {
-		return romanToDigit(value);
-	}
-	
-	return digitToRoman(number);
+	return romanToDigit(value);
+    }
+    return digitToRoman(number);
 };
